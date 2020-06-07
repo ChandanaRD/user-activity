e0512577c52ff94923aae92025abeae9c2af5a54
ChandanaRD <deshmukh.chandana@gmail.com>
2020-06-08 01:54:24 +0530

    deployment

--- activityPeriod/settings.py
+++ activityPeriod/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&s9la!s1b%p%zh8n^68^dx#3!^ytzv2wa4mlraih!!i$5m6fmv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'user-activity-2020.herokuapp.com']
 
